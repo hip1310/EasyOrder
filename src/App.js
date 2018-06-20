35138a2dd20eb9e3c229492b767efeb2b54cac26
Harsh Patel <harshpatel131090@gmail.com>
2018-06-20 17:18:02 -0400

    Initial commit with create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Going to code a new app. Excited! Well nothing to see here at the moment.
+      </div>
+    )
+  }
+}
+
+export default App
